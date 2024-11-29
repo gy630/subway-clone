f57f75b830789ee519ff0767abcc1a20cc2bfbc7
gy630 <geonye630@naver.com>
2024-11-29 15:18:41 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.main_slider', {
+  // Optional parameters
+  direction:'horizontal',
+  loop: true,
+
+  // If we need pagination
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
