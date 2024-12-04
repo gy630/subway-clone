32fd75e555aa3bc3665d80311e0b9a9d1155ce97
gy630 <geonye630@naver.com>
2024-12-04 14:43:54 +0900

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,16 +1,34 @@
 const swiper = new Swiper('.main_slider', {
   // Optional parameters
   direction:'horizontal',
+
   loop: true,
 
-  // If we need pagination
-
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
 
-
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
+
+
+
+
+/* gnb - sub menu */
+const $header = document.querySelector('header')
+const $gnb_wrap = document.querySelector('.gnb_wrap')
+const $sub_menu = document.querySelector('.sub_menu')
+
+
+$gnb_wrap.addEventListener('mouseover',()=>{
+  $header.classList.add('on')
+})
+
+$gnb_wrap.addEventListener('mouseleave',()=>{
+  $header.classList.remove('on')
+})
+
